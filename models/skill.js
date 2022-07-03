6a67fcab6afbab434acbc979e165e6d8d046444b
Gaurav Singh <gauravkorlasingh@gmail.com>
2022-07-03 10:17:52 +0530

    Updated skill model to include pow and personal endorsements

--- models/skill.js
+++ models/skill.js
@@ -3,7 +3,9 @@ var Schema = mongoose.Schema
 
 var SkillSchema = new Schema({
     name: { type: String, required: true, unique: true },
-    relatedSkills: [{ type: Schema.Types.ObjectId, ref: 'Skill' }]
+    relatedSkills: [{ type: Schema.Types.ObjectId, ref: 'Skill' }],
+    proofOfWork: { type: String, required: true, default: "" },
+    personalEndorsement: { type: String, required: true, default: "" }
 })
 
 module.exports = mongoose.model('Skill', SkillSchema)
