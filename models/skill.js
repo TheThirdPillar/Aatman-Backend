9ed36d203b8282c71a01988cd2475d4468dba722
Gaurav Singh <gauravkorlasingh@gmail.com>
2022-07-04 22:59:17 +0530

    Updated skill model to include skill level

--- models/skill.js
+++ models/skill.js
@@ -5,7 +5,8 @@ var SkillSchema = new Schema({
     name: { type: String, required: true, unique: true },
     relatedSkills: [{ type: Schema.Types.ObjectId, ref: 'Skill' }],
     proofOfWork: { type: String, required: true, default: "" },
-    personalEndorsement: { type: String, required: true, default: "" }
+    personalEndorsement: { type: String, required: true, default: "" },
+    skillLevel: { type: Number, default: 0},
 })
 
 module.exports = mongoose.model('Skill', SkillSchema)
